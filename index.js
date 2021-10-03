da4bae6686a44ea3132c3ce4b63906ad54e23fd4
Chance Hudson <jchancehud@gmail.com>
2021-10-03 16:50:41 -0500

    feat: basic channel management

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
 require('dotenv').config()
 const especial = require('especial')
+const ethers = require('ethers')
+const { ScorchedABI } = require('scorched')
+const ChannelManager = require('./src/channel_manager')
 
 const app = especial()
 
+const {
+  SUGGESTER_ADDRESS,
+  SCORCHED_ADDRESS,
+  ASSET_HOLDER_ADDRESS,
+  ADJUDICATOR_ADDRESS,
+  CHALLENGE_DURATION,
+} = process.env
+if (!SUGGESTER_ADDRESS) {
+  console.log('No SUGGESTER_ADDRESS configured')
+  process.exit(1)
+}
+
+// try to load the scorched contract, do some sanity checks
+
 app.handle('info', (data, send, next) => {
   send({
-    contract: '0x',
-    suggesterAddress: '0x',
+    version: 0,
+    contracts: {
+      scorched: SCORCHED_ADDRESS,
+      assetHolder: ASSET_HOLDER_ADDRESS,
+      adjudicator: ADJUDICATOR_ADDRESS,
+    },
+    suggester: SUGGESTER_ADDRESS,
   })
+})
+
+app.handle('channel.retrieve', (data, send, next) => {
+  if (!data.asker) {
+    send(1, 'Invalid asker address')
+    return
+  }
+  const channel = ChannelManager.channelForAsker(data.asker)
+  send(channel)
+})
+
+app.handle('channel.messages', (data, send, next) => {
+  if (!data.asker) {
+    send(1, 'Invalid asker address')
+    return
+  }
+  const messages = ChannelManager.retrieveMessages(
+    data.channelId,
+    data.asker,
+    data.start || 0,
+    data.count || 50,
+  )
+  send(messages)
+})
+
+app.handle('channel.subscribe', (data, send, next, ws) => {
+  const {
+    channelId,
+    owner,
+    subscriptionId,
+  } = data
+  const id = ChannelManager.listenToChannel(data.channelId, data.owner, (message) => {
+    // a message has been received
+    app.broadcastOne(subscriptionId, message, ws)
+  })
+  ws.on('close', () => ChannelManager.removeListener(id))
+  send()
 })
 
 const server = app.listen(4000, () => {
---
+++ src/channel_manager.js
@@ -0,0 +1,156 @@
+const ethers = require('ethers')
+const {
+  getChannelId,
+  signState,
+  getFiexedPart,
+  getVariablePart,
+  createOutcome,
+} = require('@statechannels/nitro-protocol')
+// A singleton for managing state info between suggesters and askers
+
+const {
+  SUGGESTER_ADDRESS,
+  SCORCHED_ADDRESS,
+  CHALLENGE_DURATION,
+  ASSET_HOLDER_ADDRESS,
+  RPC_URL,
+} = process.env
+
+// Types of messages sent
+const messageTypes = {
+  TEXT: 0,
+  CHANNEL_CREATED: 1,
+  CHANNEL_SIGNATURE: 2,
+  NEW_STATE: 3,
+}
+
+const normalizeAddress = (addr) => `0x${addr.replace('0x', '').toLowerCase()}`
+
+class ChannelManager {
+  channelIds = {}
+  channelIdsByAsker = {}
+  channelsById = {}
+  latestNonce = 0
+  channelListenersById = {}
+
+  constructor(filepath, rpcAddr) {
+
+  }
+
+  _createOutcome(balances) {
+    const keyedBalances = {}
+    for (const address of Object.keys(balances)) {
+      keyedBalances[ethers.utils.hexZeroPad(address, 32)] = balances[address]
+    }
+    const allocation = []
+    for (const key of Object.keys(keyedBalances)) {
+      allocation.push({
+        destination: key,
+        amount: keyedBalances[key],
+      })
+    }
+    return [
+      {
+        assetHolderAddress: ASSET_HOLDER_ADDRESS,
+        allocationItems: allocation,
+      }
+    ]
+  }
+
+  channelForAsker(_askerAddress) {
+    const askerAddress = normalizeAddress(_askerAddress)
+    if (this.channelIdsByAsker[askerAddress]) {
+      return this.channelsById[this.channelIdsByAsker[askerAddress]]
+    }
+    return this.createChannelForAsker(askerAddress)
+  }
+
+  createChannelForAsker(_askerAddress) {
+    const askerAddress = normalizeAddress(_askerAddress)
+    const channelNonce = ++this.latestNonce
+    const chainId = 4
+    const participants = [askerAddress, SUGGESTER_ADDRESS]
+    const channelConfig = {
+      chainId,
+      channelNonce,
+      participants,
+    }
+    const startingState = {
+      isFinal: false,
+      channel: channelConfig,
+      outcome: this._createOutcome({
+        [askerAddress]: 10,
+        [SUGGESTER_ADDRESS]: 10,
+        [ethers.constants.AddressZero]: 0,
+      }),
+      appDefinition: SCORCHED_ADDRESS,
+      appData: ethers.constants.HashZero,
+      challengeDuration: CHALLENGE_DURATION ?? 24 * 60 * 60 * 1000,
+      turnNum: 0,
+    }
+    const channelId = getChannelId(channelConfig)
+    const channelCreatedMessage = {
+      type: messageTypes.CHANNEL_CREATED,
+      channelId,
+      startingState,
+    }
+    // suggester needs to sign first, then asker
+    const channel = {
+      id: channelId,
+      participants,
+      states: [startingState],
+      messages: [],
+    }
+    this.channelsById[channelId] = channel
+    this.channelIdsByAsker[askerAddress] = channelId
+    this.sendMessage(channel.id, channelCreatedMessage)
+    return channel
+  }
+
+  retrieveMessages(channelId, _owner, start, count) {
+    const owner = normalizeAddress(_owner)
+    if (!this.channelsById[channelId]) return []
+    const { messages } = this.channelsById[channelId]
+    return messages
+  }
+
+  sendMessage(channelId, _message) {
+    if (!_message.type) throw new Error('Invalid message type')
+    if (!this.channelsById[channelId]) throw new Error('Channel does not exist')
+    const message = {
+      ..._message,
+      timestamp: +new Date(),
+    }
+    this.channelsById[channelId].messages.unshift(message)
+    const listeners = this.channelListenersById[channelId] || []
+    for (const fn of listeners) {
+      if (!fn) continue
+      try {
+        fn(message)
+      } catch (err) {
+        console.log(err)
+        console.log('Uncaught error in channel listener callback')
+      }
+    }
+  }
+
+  listenToChannel(channelId, _owner, cb) {
+    const owner = normalizeAddress(_owner)
+    if (!this.channelsById[channelId])
+      throw new Error('The specific channel does not exist')
+    if (typeof cb !== 'function')
+      throw new Error('Channel listener callback must be a function')
+    if (!this.channelListenersById[channelId])
+      this.channelListenersById[channelId] = []
+    this.channelListenersById[channelId].push(cb)
+    // the id is the index
+    return this.channelListenersById[channelId].length - 1
+  }
+
+  removeListener(channelId, index) {
+    if (!this.channelListenersById[channelId]) return
+    this.channelListenersById[channelId][index] = undefined
+  }
+}
+
+module.exports = new ChannelManager()
